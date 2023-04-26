64092de3820b16ffdd17546b80ea443e4c37c3ea
abhijeet srivastava <abhijeet.srivastava@morganstanley.com>
2023-04-27 03:34:08 +0530

    remove from basket

--- src/reducer.js
+++ src/reducer.js
@@ -2,7 +2,7 @@ export const initialState = {
     basket: []
 }
 export const getBasketTotal = (basket) => {
-    return(basket?.reduce((amount, item) => item.price + amount, 0));
+    return (basket?.reduce((amount, item) => item.price + amount, 0));
 }
 
 const reducer = (state, action) => {
@@ -12,6 +12,26 @@ const reducer = (state, action) => {
                 ...state,
                 basket: [...state.basket, action.item],
             }
+        case "REMOVE_FROM_BASKET":
+            const index = state.basket.findIndex(
+                (basketItem) => basketItem.id === action.id
+            );
+
+            let newBasket = [...state.basket];
+
+            if (index >= 0) {
+                newBasket.splice(index, 1);
+            } else {
+                console.warn(
+                    `Can't remove product(id: ${action.id}) as its not in the basket!`
+                )
+            }
+            return {
+                ...state,
+                basket: newBasket
+            }
+        default:
+            return state;
     }
 
 }
